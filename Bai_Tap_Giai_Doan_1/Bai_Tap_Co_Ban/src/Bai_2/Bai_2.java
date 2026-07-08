adf2ec8b97dc51693baa0e087b176f146c0f68b7
Chinh-NTC <chinh251106@gmail.com>
2026-07-08 22:48:00 +0700

    Bai tap giai doan 1 - Bai tap co ban - Bai 2

---
+++ Bai_Tap_Giai_Doan_1/Bai_Tap_Co_Ban/src/Bai_2/Bai_2.java
@@ -0,0 +1,13 @@
+// 2. Viết chương trình in ra 3 dòng: tên trường, tên khoa, nghành học - mỗi thông tin một dòng
+
+package Bai_2;
+
+public class Bai_2
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Trường: Đại học Sư Phạm Kỹ Thuật - Đại học Đà Nẵng");
+        System.out.println("Khoa: Công nghệ số");
+        System.out.println("Nghành: Công nghệ thông tin");
+    }
+}
