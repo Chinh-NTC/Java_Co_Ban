5a6718198f86aad9a53f97a144181e15cd1b8e8d
Chinh-NTC <chinh251106@gmail.com>
2026-07-08 22:53:03 +0700

    Bai tap giai doan 1 - Bai tap co ban - Bai 3

---
+++ Bai_Tap_Giai_Doan_1/Bai_Tap_Co_Ban/src/Bai_3/Bai_3.java
@@ -0,0 +1,11 @@
+package Bai_3;
+
+public class Bai_3
+{
+    public static void main(String[] args)
+    {
+        // 3. Sửa lỗi cho đoạn code System.out.println(Xin chao)
+        // Thêm dấu "" cho chuỗi Xin chao và thêm dấu ; vào cuối đoạn code
+        System.out.println("Xin chao");
+    }
+}
