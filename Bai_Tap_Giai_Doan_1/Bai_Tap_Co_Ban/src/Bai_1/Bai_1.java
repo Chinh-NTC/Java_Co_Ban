2e78601062f96024609162970a409dbaaeec2140
Chinh-NTC <chinh251106@gmail.com>
2026-07-08 22:25:50 +0700

    Bai tap giai doan 1 - Bai tap co ban - Bai1

---
+++ Bai_Tap_Giai_Doan_1/Bai_Tap_Co_Ban/src/Bai_1/Bai_1.java
@@ -0,0 +1,12 @@
+// 1. Viết chương trình in ra họ tên và lớp học
+
+package Bai_1;
+
+public class Bai_1
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Họ và tên: Nguyễn Tấn Chinh");
+        System.out.println("Lớp: 24T3");
+    }
+}
