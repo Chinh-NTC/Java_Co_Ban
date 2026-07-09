bd16156539cc15276c24559509c8914485611d15
Chinh-NTC <chinh251106@gmail.com>
2026-07-09 07:54:25 +0700

    Bai tap giai doan 1 - Bai tap co ban - Bai 4

---
+++ Bai_Tap_Giai_Doan_1/Bai_Tap_Co_Ban/src/Bai_4/Bai_4.java
@@ -0,0 +1,13 @@
+// 4. Viết chương trình dùng System.out.print (không xuống dòng) để in ra: "Java-" "Backend-" "Developer" trên cùng một dòng
+
+package Bai_4;
+
+public class Bai_4
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Java-");
+        System.out.print("Backend-");
+        System.out.print("Developer");
+    }
+}
