c64d08da131fbc3b17219ba625f7a86265f0408d
Chinh-NTC <chinh251106@gmail.com>
2026-07-09 08:03:10 +0700

    Bai tap giai doan 1 - Bai tap trung binh - Bai 1

---
+++ Bai_Tap_Giai_Doan_1/Bai_Tap_Trung_Binh/src/Bai_1/Bai_1.java
@@ -0,0 +1,16 @@
+// 1. Viết chương trình in ra một "bảng thông tin cá nhân" đơn giản dạng khung chữ (dùng các kỹ tự -, |, + để vẽ khung) gồm họ tên, MSSV, lớp
+
+package Bai_1;
+
+public class Bai_1
+{
+    public static void main(String args[])
+    {
+        System.out.println("BẢNG THÔNG TIN CÁ NHÂN");
+        System.out.println("----------------------------");
+        System.out.println("Họ và tên | Nguyễn Tấn Chinh");
+        System.out.println("MSSV | 2415053122302");
+        System.out.println("Lớp | 24T8");
+        System.out.println("----------------------------");
+    }
+}
