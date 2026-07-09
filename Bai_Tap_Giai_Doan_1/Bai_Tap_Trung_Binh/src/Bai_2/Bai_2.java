0a9414cbc7502aa56f89c09f8f8ddc8430eaa6e2
Chinh-NTC <chinh251106@gmail.com>
2026-07-09 08:07:17 +0700

    Bai tap giai doan 1 - Bai tap trung binh - Bai 2

---
+++ Bai_Tap_Giai_Doan_1/Bai_Tap_Trung_Binh/src/Bai_2/Bai_2.java
@@ -0,0 +1,13 @@
+// 2. Dùng System.out.printf để in ra điểm trung bình học kỳ giả định là 8.256, yêu cầu chỉ hiện 2 số lẻ sau dấu phẩy
+
+package Bai_2;
+
+
+public class Bai_2
+{
+    public static void main(String args[])
+    {
+        double dtb = 8.256;
+        System.out.printf("Điểm trung bình học kỳ: %.2f", dtb);
+    }
+}
