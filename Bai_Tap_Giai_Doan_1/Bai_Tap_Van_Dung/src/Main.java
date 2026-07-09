17211b11eb415da2ecea20bb06e913f1720a0322
Chinh-NTC <chinh251106@gmail.com>
2026-07-09 08:24:11 +0700

    Bai tap giai doan 1 - Bai tap van dung

--- Bai_Tap_Giai_Doan_1/Bai_Tap_Van_Dung/src/Main.java
+++ Bai_Tap_Giai_Doan_1/Bai_Tap_Van_Dung/src/Main.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+/*
+Viết một chương trình JAVA in ra 'CV giới thiệu bản thân" ngắn gồm:
+họ tên,  nghành học, mục tiêu nghề nghiệp, trình bày đẹp mứt bằng
+nhiều dòng System.out.println và một số ký tự trang trí (=, -, +)
+ */
 
-public class Main {
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("CV GIỚI THIỆU BẢN THÂN");
+        System.out.println("-----------------------------");
+        System.out.println("Họ tên - Nguyễn Tấn Chinh");
+        System.out.println("Chuyên nghành - Công nghệ thông tin");
+        System.out.println("Mục tiêu nghề nghiệp - Trở thành một developer JAVA");
+        System.out.println("-----------------------------");
+    }
 }
