1a7a482896bc543c80944adb9c8f1a64479649a7
Chinh-NTC <chinh251106@gmail.com>
2026-07-09 08:12:55 +0700

    Bai tap giai doan 1 - Bai tap trung binh - Bai 3

---
+++ Bai_Tap_Giai_Doan_1/Bai_Tap_Trung_Binh/src/Bai_3/Bai_3.java
@@ -0,0 +1,6 @@
+// 3. Giải thích sự khác nhau giữa JDK, JRE, JVM
+/*
+JDK là bộ công cụ để phát triển chương trình JAVA, bao gồm JRE + compiler + các công cụ khác
+JRE là môi trường để chạy chương trình JAVA, bao gồm JVM + các library cần thiết
+JVM là máy ảo thực thi mã bytecode của JAVA. Đây là bộ máy giúp code JAVA chạy được trên mọi hệ điều hành
+ */
