8a872d9194e5aea09ad4f0326c5decc8f2bb87a0
Chinh-NTC <chinh251106@gmail.com>
2026-07-10 14:07:07 +0700

    Bai tap giai doan 3 - Bai tap co ban - Bai 3

---
+++ Bai_Tap_Giai_Doan_3/Bai_Tap_Co_Ban/src/bai3/Bai3.java
@@ -0,0 +1,15 @@
+/*
+3. Viết chương trình dùng vòng for in ra các số từ 1 đến 20
+ */
+package bai3;
+
+public class Bai3 {
+    public static void main(String[] args) {
+        // Khai báo biến
+        int n = 20;
+
+        for (int i = 1; i <= n; i++){
+            System.out.print(i + " ");
+        }
+    }
+}
