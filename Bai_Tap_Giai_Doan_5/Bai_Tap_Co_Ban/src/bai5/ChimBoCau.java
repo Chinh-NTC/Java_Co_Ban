1937594d4bede28b91277ec67707cb573ee56a21
Chinh-NTC <chinh251106@gmail.com>
2026-07-19 02:12:49 +0700

    Bai tap giai doan 5 - Bai tap co ban - Bai 5

---
+++ Bai_Tap_Giai_Doan_5/Bai_Tap_Co_Ban/src/bai5/ChimBoCau.java
@@ -0,0 +1,8 @@
+package bai5;
+
+public class ChimBoCau implements CoTheBay {
+    @Override
+    public void bay() {
+
+    }
+}
---
+++ Bai_Tap_Giai_Doan_5/Bai_Tap_Co_Ban/src/bai5/CoTheBay.java
@@ -0,0 +1,11 @@
+package bai5;
+
+/**
+ * 5. Viết một interface CoTheBay với method bay(), cho một class ChimBoCau implements interface đó.
+ * @author Nguyễn Tấn Chinh
+ * @version 1.0
+ * @since 2026-07-19
+ */
+public interface CoTheBay {
+    void bay();
+}
