cfebba32a3d7aa67322f3ae7085e8a976f02debe
Chinh-NTC <chinh251106@gmail.com>
2026-07-19 02:04:08 +0700

    Bai tap giai doan 5 - Bai tap co ban - Bai 4

---
+++ Bai_Tap_Giai_Doan_5/Bai_Tap_Co_Ban/src/bai4/Cho.java
@@ -0,0 +1,8 @@
+package bai4;
+
+public class Cho extends DongVat{
+    @Override
+    public void keu(){
+        System.out.println("Gau gau!");
+    }
+}
---
+++ Bai_Tap_Giai_Doan_5/Bai_Tap_Co_Ban/src/bai4/DongVat.java
@@ -0,0 +1,13 @@
+package bai4;
+
+/**
+ * 4. Viết 2 class: DongVat (class Cha) có method keu(), va Cho(class con) override lại method keu() để in ra "Gau gau!".
+ * @author Nguyễn Tấn Chinh
+ * @version 1.0
+ * @since 2026-07-19
+ */
+public abstract class DongVat {
+
+    // Method kêu
+    public abstract void keu();
+}
